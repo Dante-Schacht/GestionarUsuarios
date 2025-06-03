6cd06de1f4d31073ae96f22421bee424c6c5579b
Dante Schacht <dantesch10@gmail.com>
2025-06-03 19:08:33 -0400

    Primer commit

---
+++ src/main/java/com/GestionarUsuarios/GestionarUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.GestionarUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionarUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionarUsuariosApplication.class, args);
+	}
+
+}

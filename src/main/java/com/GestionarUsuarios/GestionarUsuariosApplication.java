fdba39efbd1cd4e78798fe71ffe3ef5978171f5c
natanael reveco <reveconatanael03@gmail.com>
2025-06-03 19:16:04 -0400

    se agrego la api gestionar usuarios 1.0

--- src/main/java/com/GestionarUsuarios/GestionarUsuariosApplication.java
+++ src/main/java/com/GestionarUsuarios/GestionarUsuariosApplication.java
@@ -1,13 +1,15 @@
 package com.GestionarUsuarios;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.GestionarUsuarios"})
 public class GestionarUsuariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionarUsuariosApplication.class, args);
 	}
+	
 
 }

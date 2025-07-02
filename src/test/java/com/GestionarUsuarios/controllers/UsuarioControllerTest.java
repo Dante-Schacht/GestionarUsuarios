63ffb791d8617d1e5d178e4c6c06ce2dc94c56ac
natanael reveco <reveconatanael03@gmail.com>
2025-07-02 00:24:10 -0400

    se hizo una prueba con junit

---
+++ src/test/java/com/GestionarUsuarios/controllers/UsuarioControllerTest.java
@@ -0,0 +1,30 @@
+package com.GestionarUsuarios.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class UsuarioControllerTest {
+    @Test
+    void testCrear() {
+
+    }
+
+    @Test
+    void testEditarUsuario() {
+
+    }
+
+    @Test
+    void testEliminar() {
+
+    }
+
+    @Test
+    void testGetAll() {
+
+    }
+
+    @Test
+    void testGetById() {
+
+    }
+}
